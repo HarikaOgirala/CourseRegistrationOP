b2262ea984de2a55260d5d2a7d71baa8624221f2
NagaHarika <n.ogirala@my.ccsu.edu>
2021-04-28 19:17:00 -0400

    course Details page updated

--- src/app/courses.ts
+++ src/app/courses.ts
@@ -2,13 +2,13 @@ export class Courses {
     id!: number;
     courseNumber!: string;
     courseName!: string;
-    courseDescription!: string; 
+    courseDesc!: string; 
     semester!: string;
-    courseCRN!: string;
+    crn!: string;
     type!: string;
     preRequisites!: string;
     partOfDay!: string;
-    timings!: string;
+    time!: string;
     day!: string;
     faculty!: string;
     active!: boolean;
